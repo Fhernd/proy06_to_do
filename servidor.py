2c4484d779a87678834a44960e6f09f2ccf49bae
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2023-10-04 18:44:59 -0500

    Punto de entrada de la aplicación

--- servidor.py
+++ servidor.py
@@ -1,1 +1,5 @@
-print('Proyecto 06 - To-Do')
+from app import app  # Asumiendo que la instancia de Flask está en __init__.py de la carpeta 'app'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
