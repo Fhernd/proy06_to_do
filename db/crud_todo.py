00fd5d8433c94596f3399d5228bbf2083fb84d72
John Ortiz Ordoñez <jf.ortiz45@uniandes.edu.co>
2024-02-12 18:18:52 -0500

    Tratando de resolver problema de SQL

--- db/crud_todo.py
+++ db/crud_todo.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from db.conexion import crear_conexion
 
 
@@ -8,12 +10,24 @@ def crear_tarea(tarea):
     - tarea: str -- Descripción de la tarea
     """
     try:
+        print('antes de crear conexion')
         conexion = crear_conexion()
+        print('después de crear conexion')
         cursor = conexion.cursor()
-        cursor.execute('INSERT INTO todo (Titulo, TareaTerminada) VALUES (?)', (tarea['titulo'], tarea['TareaTerminada']))
+        
+        fecha_tarea = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        fecha_modificacion = fecha_tarea
+        print(fecha_tarea, fecha_modificacion)
+        
+        
+        print('antes de execute')
+        cursor.execute('INSERT INTO todo (Titulo, FechaTarea, TareaTerminada, FechaModificacion) VALUES (?, ?, ?, ?)', (tarea['titulo'], fecha_tarea, tarea['TareaTerminada'], fecha_modificacion))
+        print('antes del commit')
         conexion.commit()
+        print('después del commit')
         conexion.close()
         
         return True
     except Exception as e:
+        print(e)
         return False
